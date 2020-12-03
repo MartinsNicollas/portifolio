c841c1e30e76075dc9a1c406374db62ce2963b3e
Nicollas <73492024+MartinsNicollas@users.noreply.github.com>
2020-12-03 18:01:48 -0300

    Add files via upload

---
+++ dist/script.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var hamburguer = document.querySelector(".hamburguer");
+hamburguer.addEventListener("click", function () {
+  document.querySelector("sidebar").classList.toggle("shadow-manu");
+});
